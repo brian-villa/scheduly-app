fda81602d5b8375a0bead4cc95a16c36aa1b01f1
brian-villa <brian.rj99@yahoo.com.br>
2024-07-25 15:13:03 +0100

    initial files

---
+++ scheduly-backend/src/main/java/com/example/scheduly/SchedulyApplication.java
@@ -0,0 +1,13 @@
+package com.example.scheduly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulyApplication.class, args);
+	}
+
+}
